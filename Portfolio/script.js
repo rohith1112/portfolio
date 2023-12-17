7f92a5289798d174937a16108e46a56124de4613
rohith1112 <97204786+rohith1112@users.noreply.github.com>
2023-12-17 06:19:56 -0800

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,140 @@
+        function showSidebar() {
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.classList.add('show');
+        }
+        function hideSidebar() {
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.classList.remove('show');
+        }
+        document.addEventListener('DOMContentLoaded', function () {
+            const sidebar = document.querySelector('.sidebar');
+        
+            function showSidebar() {
+                sidebar.classList.add('show');
+            }
+        
+            function hideSidebar() {
+                sidebar.classList.remove('show');
+            }
+        
+            // Toggle sidebar visibility on button click
+            const menuButton = document.querySelector('.menu-button');
+            menuButton.addEventListener('click', function (event) {
+                event.stopPropagation(); // Prevents the body click event from being triggered
+                showSidebar();
+            });
+        
+            // Hide sidebar when clicking outside of it
+            document.body.addEventListener('click', function (event) {
+                const isMenuButton = event.target.closest('.menu-button');
+                const isSidebar = event.target.closest('.sidebar');
+        
+                if (!isMenuButton && !isSidebar) {
+                    hideSidebar();
+                }
+            });
+        
+            // Close sidebar when clicking on the hide button
+            const hideButton = document.querySelector('.sidebar .hide-button');
+            hideButton.addEventListener('click', function (event) {
+                event.stopPropagation(); // Prevents the body click event from being triggered
+                hideSidebar();
+            });
+        });
+        
+        document.addEventListener("DOMContentLoaded", function () {
+            // Smooth scrolling for anchor links
+            const scrollLinks = document.querySelectorAll('a[href^="#"]');
+            scrollLinks.forEach((link) => {
+              link.addEventListener("click", (e) => {
+                e.preventDefault();
+          
+                const targetId = link.getAttribute("href").substring(1);
+                const targetElement = document.getElementById(targetId);
+          
+                if (targetElement) {
+                  window.scrollTo({
+                    top: targetElement.offsetTop - 60, // Adjust the offset as needed
+                    behavior: "smooth",
+                  });
+                }
+              });
+            });
+          });
+          
+          function showSidebar() {
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.classList.add('show');
+          }
+          
+          function hideSidebar() {
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.classList.remove('show');
+          }
+
+
+          // JavaScript to handle the popup functionality
+document.addEventListener("DOMContentLoaded", function () {
+  var popupContainer = document.getElementById("popup-container");
+  var closeBtn = document.getElementById("close-btn");
+  var knowMoreBtn = document.querySelector(".knowmore");
+
+  // Function to open the popup
+  function openPopup() {
+      popupContainer.style.display = "block";
+  }
+
+  // Function to close the popup
+  function closePopup() {
+      popupContainer.style.display = "none";
+  }
+
+  // Event listener for the "Know More" button
+  knowMoreBtn.addEventListener("click", openPopup);
+
+  // Event listener for the close button
+  closeBtn.addEventListener("click", closePopup);
+
+  // Event listener to close the popup when clicking outside
+  window.addEventListener("click", function (event) {
+      if (event.target === popupContainer) {
+          closePopup();
+      }
+  });
+});
+
+
+function openResume() {
+  // Change the file path accordingly
+  var filePath = 'resume.pdf';
+
+  // Open the file in a new window or tab
+  window.open(filePath, '_blank');
+}
+
+
+$(document).ready(function () {
+  // Toggle class on hover for desktop
+  $('.skill-box').hover(
+    function () {
+      $(this).toggleClass('active');
+    },
+    function () {
+      $(this).toggleClass('active');
+    }
+  );
+
+  // Toggle class on click for mobile
+  $('.skill-box').on('click', function () {
+    $(this).toggleClass('active').siblings().removeClass('active');
+  });
+
+  // Prevent link clicks on mobile to avoid navigation
+  $('.skill-box a').on('click', function (e) {
+    if ($(this).parent().hasClass('active')) {
+      e.preventDefault();
+    }
+  });
+});
+
+
